b6c71baba49e7730c6c7be486f9bca358525c4ef
Nicolas Penin <nicolas.penin@dragon-angel.fr>
2018-03-18 12:31:16 +0100

    reimplementing main feature removed self dependency

--- src/server/index.ts
+++ src/server/index.ts
@@ -2,17 +2,27 @@ import * as akala from '@akala/server';
 import * as vm from 'vm';
 import * as url from 'url';
 import * as path from 'path';
-import * as chat from '@domojs/chat';
+import * as language from './language'
 
+export = language;
 
-const debug = akala.log('domojs:chat:date');
+const debug = akala.log('domojs:chat');
 
-akala.worker.createClient('chat').then(function (client)
+var interpreters = [];
+akala.register('interpreters', interpreters);
+
+akala.injectWithName(['$master', '$isModule'], function (master: akala.worker.MasterRegistration, ismodule: akala.worker.IsModule)
 {
-    chat.meta.createServerProxy(client).register({ path: path.resolve(__dirname, './interpreter'), name: 'date' });
-});
+    if (ismodule('@domojs/chat'))
+        master(__dirname, './master');
 
-akala.injectWithName(['$master'], function (master: akala.worker.MasterRegistration)
-{
-    master(__dirname, './master');
+    akala.worker.createClient('chat').then((client) =>
+    {
+        language.meta.createClient(client)({
+            receive: function (interpreter: language.Language)
+            {
+                
+            }
+        })
+    })
 })();
